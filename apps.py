8471c21faf6e3b5a5958c8705b9687f20e9f4613
Josh <josh@catalpa.io>
2021-04-01 12:29:27 +0900

    initialize a Django app in repo

---
+++ apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TagTranslationsConfig(AppConfig):
+    name = 'tag_translations'
---
+++ models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+class TranslationTargetQuerySet(models.Manager):
+
+    def catalog(self):
+        """
+
+        """
+        # Group all of the "plural" forms into an array
+        pass
+
+
+class TranslationSource(models.Model):
+    """
+    A "Translated" string may include
+    """
+    parent = models.ForeignKey("self", on_delete = models.CASCADE)
+    msgid = models.CharField(max_length=512, help_text="The message text to translate from", unique=True)
+    msgid_plural = models.CharField(max_length=512, help_text="Optional plural form of the message")
+    comment_for_translators = models.CharField(max_length=2000)
+
+
+class TranslatedMessage(models.Model):
+    msg = models.ForeignKey(TranslationSource)
+    msgstr = models.CharField(max_length=512)
+    msgstr_plurality = models.IntegerField(nunll=True, blank=True, help_text="Which plural form this translation refers to")
+    language = models.CharField(max_length=5)
+
+
+class CommonText(models.Model):
+    """
+    Holds translations of common components which may be renamed between
+    projects. For example a mohinga 'Activity' is a Dird 'Project' and a Hamutuk 'Program'.
+    """
+    concept = models.CharField(max_length=128)
+    description = models.TextField()
+    translated_string = models.ForeignKey(TranslationSource, to_field='msgid')
