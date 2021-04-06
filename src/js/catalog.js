ddeabaddcdd267677e3ba14de2c8c564e11dab4e
Josh <josh@catalpa.io>
2021-04-06 23:35:54 +0900

    'gettext' functions public interface, with tests

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -1,129 +1,152 @@
-const csep = '\x04'; // The separator between "context" marker and "text" value, as per Django
-const contextualize = (context, text) => `${context}${csep}${text}` //Return a 'contextualized' string, appending context and marker to key
+const csep = "\x04"; // The separator between "context" marker and "text" value, as per Django
+const contextualize = (context, text) => `${context}${csep}${text}`; //Return a 'contextualized' string, appending context and marker to key
 /*
 Type checking helps to determine whether we found a value
 in the dictionary and whether there are plural forms of this string
 */
-const is_undef = (value) => typeof(value) == 'undefined';
-const is_str = (value) => typeof(value) == 'string';
-const is_arr = (value) => Array.isArray(value)
+const is_undef = (value) => typeof value == "undefined";
+const is_str = (value) => typeof value == "string";
+const is_arr = (value) => Array.isArray(value);
 
 /*
 Determining if a given count should use a plural form of a word or not
 Plural / Singular forms are passed as an array
 */
-const pluralidx = (count) => count == 1 ? 0 : 1
+const pluralidx = (count) => (count == 1 ? 0 : 1);
 
-const gettext = (catalog, msgid) => {
-    /*
+const _gettext = (catalog, msgid) => {
+  /*
     For the simplest case of a single word or phrase translation
     this looks up the w/p from the catalog
     */
-    let value = catalog[msgid];
-    if (is_undef(value)){
-        return gettext_noop(msgid);
-    } else if (is_str(value)){
-        return value;
-    } else if (is_arr(value)){
-        return value[0]
-    }
+  let value = catalog[msgid];
+  if (is_undef(value)) {
+    return _gettext_noop(msgid);
+  } else if (is_str(value)) {
+    return value;
+  } else if (is_arr(value)) {
+    return value[0];
+  }
 };
 
-const ngettext = (catalog, msgid, plural, count) => {
-    /*
+const _ngettext = (catalog, singular, plural, count) => {
+  /*
     plural is a fallback value if not found in the dictionary
     */
-    let value = catalog[msgid];
-    if (is_undef(value)){
-        return (count == 1 || !plural) ? gettext_noop(msgid) : plural;
-    } else if (is_str(value)){
-        return value;
-    } else if (is_arr(value)){
-        return value[pluralidx(count)];
-    }
-};
-
-const gettext_noop = (msgid) => msgid;
-
-const pgettext = (catalog, context, msgid) => {
-    let value = gettext(catalog, contextualize(context, msgid));
-
-    if (value.indexOf(csep) != -1) {
-        value = msgid;
-    }
+  let value = catalog[singular];
+  if (is_undef(value)) {
+    return count == 1 || !plural ? _gettext_noop(singular) : plural;
+  } else if (is_str(value)) {
     return value;
+  } else if (is_arr(value)) {
+    return value[pluralidx(count)];
+  }
 };
 
-const npgettext = (catalog, context, msgid, plural, count) => {
-    let value = ngettext(
-        catalog,
-        contextualize(context, msgid),
-        contextualize(context, plural),
-        count
-    );
-    if (value.indexOf(csep) != -1) {
-        value = ngettext(catalog, msgid, plural, count);
-    }
-    return value;
+const _gettext_noop = (msgid) => msgid;
+
+const _pgettext = (catalog, context, msgid) => {
+  let value = _gettext(catalog, contextualize(context, msgid));
+
+  if (value.indexOf(csep) != -1) {
+    value = msgid;
+  }
+  return value;
 };
 
-const do_named_interpolate = (fmt, obj) => fmt.replace(/%\(\w+\)s/g, function(match) {return String(obj[match.slice(2,-2)])});
-const do_interpolate = (fmt, obj) => fmt.replace(/%s/g, function(match){return String(obj.shift())});
+const _npgettext = (catalog, context, singular, plural, count) => {
+  let value = _ngettext(
+    catalog,
+    contextualize(context, singular),
+    contextualize(context, plural),
+    count
+  );
+  if (value.indexOf(csep) != -1) {
+    value = _ngettext(catalog, singular, plural, count);
+  }
+  return value;
+};
+
+const do_named_interpolate = (fmt, obj) =>
+  fmt.replace(/%\(\w+\)s/g, function (match) {
+    return String(obj[match.slice(2, -2)]);
+  });
+const do_interpolate = (fmt, obj) =>
+  fmt.replace(/%s/g, function (match) {
+    return String(obj.shift());
+  });
 
 export default class Catalog {
-    constructor(content, translate_url) {
-        this.content = content;
-        this.translate_url = translate_url;
-        this.waitingTranslations = [];
-        this.timer = undefined;
-        this.timeout = 300;
-    };
+  constructor(content, translate_url) {
+    this.content = content;
+    this.translate_url = translate_url;
+    this.waitingTranslations = [];
+    this.timer = undefined;
+    this.timeout = 300;
+  }
 
-    fetchTranslations() {
-        const response = fetch(this.translate_url, {
-            method: "POST",
-            body: JSON.stringify(this.waitingTranslations),
-            headers: {
-                "Content-Type": "application/json",
-            }
-        });
-        this.waitingTranslations = [];
-        return response;
-    }
+  fetchTranslations() {
+    const response = fetch(this.translate_url, {
+      method: "POST",
+      body: JSON.stringify(this.waitingTranslations),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    this.waitingTranslations = [];
+    return response;
+  }
 
-    collect(context, msgid, plural) {
-        clearTimeout(this.timer);
-        this.waitingTranslations.push([context, msgid, plural]);
-        this.timer = setTimeout(this.fetchTranslations, this.timeout);
-    }
+  collect(context, msgid, plural) {
+    clearTimeout(this.timer);
+    this.waitingTranslations.push([context, msgid, plural]);
+    this.timer = setTimeout(this.fetchTranslations, this.timeout);
+  }
 
-    translate (context, msgid, plural, count, interpolate, interpolate_named) {
-        /*
+  translate(context, msgid, plural, count, interpolate, interpolate_named) {
+    /*
         Return the output of an appropriately chosen translation function.
         Depending on the parameters passed this will range from a simple lookup
         to something which outputs a plural string from a contextual dictionary
         */
-        const catalog = this.content || {};
-        let message;
+    const catalog = this.content || {};
+    let message;
 
-        /* Broadly speaking, go from 'most complex' to 'least complex' case */
-        if (context && msgid && !is_undef(plural) && count) { message = npgettext(catalog, context, msgid, plural, parseInt(count)) }
-        else if (msgid && !is_undef(plural) && count) { message = ngettext(catalog, msgid, plural, parseInt(count)) }
-        else if (msgid && context) { message = pgettext(catalog, context, msgid) }
-        else if (msgid) { message = gettext(catalog, msgid) }
-
-        /* Further processing by string interpolation */
-        if (interpolate_named) { message = do_named_interpolate(message, interpolate_named) }
-        else if (interpolate) { message = do_interpolate(message, interpolate) }
-        return message;
-    };
-
-    translate_opts(opts) {
-        /*Parameterized version of translate */
-        return translate (opts.context, opts.msgid, opts.plural, opts.count, opts.interpolate, opts.interpolate_named)
+    /* Broadly speaking, go from 'most complex' to 'least complex' case */
+    if (context && msgid && !is_undef(plural) && count) {
+      message = this.npgettext(context, msgid, plural, parseInt(count));
+    } else if (msgid && !is_undef(plural) && count) {
+      message = this.ngettext(msgid, plural, parseInt(count));
+    } else if (msgid && context) {
+      message = this.pgettext(context, msgid);
+    } else if (msgid) {
+      message = this.gettext(msgid);
     }
 
-    has(context, msgid) {
-        return !is_undef(this.content[context ? contextualize(context, msgid) : msgid]);
+    /* Further processing by string interpolation */
+    if (interpolate_named) {
+      message = do_named_interpolate(message, interpolate_named);
+    } else if (interpolate) {
+      message = do_interpolate(message, interpolate);
     }
-};
+    return message;
+  }
+
+  /* These have the same function signature as Django's js translation functions */
+  gettext(msgid) {
+    return _gettext(this.content, msgid);
+  }
+  ngettext(singular, plural, count) {
+    return _ngettext(this.content, singular, plural, count);
+  }
+  pgettext(context, msgid) {
+    return _pgettext(this.content, context, msgid);
+  }
+  npgettext(context, singular, plural, count) {
+    return _npgettext(this.content, context, singular, plural, count);
+  }
+
+  interpolate(fmt, obj, named) {
+    return named ? do_named_interpolate(fmt, obj) : do_interpolate(fmt, obj);
+  }
+}
--- src/js/test.js
+++ src/js/test.js
@@ -1,36 +1,118 @@
-import Catalog from './catalog.js';
-var assert = require('assert');
+import Catalog from "./catalog.js";
+var assert = require("assert");
 
 const tet = new Catalog({
-  'Activity': 'Actividade',
-  'Project': 'Projetu',
-  'verb\x04may': 'bele',
-  'month\x04may': 'maiu',
-  'animal\x04Elephant': 'Amimal abu-abu neebe boot',
-  'There is one activity': ['Iha atividade ida', 'Iha atividade barak'],
-  'foo\x04There is one activity': ['Iha foo ida', 'Iha foo barak'],
-  'bar\x04There is one activity': ['Iha bar ida', 'Iha bar barak'],
-  'There is %(total)s object. Remaining: %(count)s': ['Iha %(total)s objetu. Restu mak: %(count)s', 'Iha %(total)s objetu sira. Restu mak: %(count)s'],
-  'I caught %s pocket monster!. Remaining: %s': ["Hau kaer %s pokemon deit!. Sei livre: %s", "Hau kaer %s pokemon! Sei livre: %s"]
-})
+  Activity: "Actividade",
+  Project: "Projetu",
+  "verb\x04may": "bele",
+  "month\x04may": "maiu",
+  "animal\x04Elephant": "Amimal abu-abu neebe boot",
+  "There is one activity": ["Iha atividade ida", "Iha atividade barak"],
+  "foo\x04There is one activity": ["Iha foo ida", "Iha foo barak"],
+  "bar\x04There is one activity": ["Iha bar ida", "Iha bar barak"],
+  "There is %(total)s object. Remaining: %(count)s": [
+    "Iha %(total)s objetu. Restu mak: %(count)s",
+    "Iha %(total)s objetu sira. Restu mak: %(count)s",
+  ],
+  "There is %s object. Remaining: %s": [
+    "Iha %s objetu. Restu mak: %s",
+    "Iha %s objetu sira. Restu mak: %s",
+  ],
+  "I caught %s pocket monster!. Remaining: %s": [
+    "Hau kaer %s pokemon deit!. Sei livre: %s",
+    "Hau kaer %s pokemon! Sei livre: %s",
+  ],
+});
 
-describe("Tetun translations", function(){
-  it('should translate Activity to Actividade', () => {
-    assert.equal( tet.translate('', 'Activity'), 'Actividade');
+describe("Tetun translations", function () {
+  it("should translate Activity to Actividade", () => {
+    assert.equal(tet.translate("", "Activity"), "Actividade");
   }),
+    it("should translate contextual markers when present", () => {
+      assert.equal(tet.translate("verb", "may"), "bele");
+      assert.equal(tet.translate("month", "may"), "maiu");
+    });
 
-  it('should translate contextual markers when present', () => {
-    assert.equal(tet.translate('verb', 'may'), 'bele');
-    assert.equal( tet.translate('month', 'may'), 'maiu');
-  })
+  it("should translate interpolated positional markers when present", () => {
+    assert.equal(
+      tet.translate(
+        "",
+        "There is %(total)s object. Remaining: %(count)s",
+        "There are %(total)s objects. Remaining: %(count)s",
+        1,
+        "",
+        { total: 1, count: 10 }
+      ),
+      "Iha 1 objetu. Restu mak: 10"
+    );
+    assert.equal(
+      tet.translate(
+        "",
+        "There is %(total)s object. Remaining: %(count)s",
+        "There are %(total)s objects. Remaining: %(count)s",
+        5,
+        "",
+        { total: 5, count: 10 }
+      ),
+      "Iha 5 objetu sira. Restu mak: 10"
+    );
+  });
+  it("Should not require a plural for translation", () => {
+    assert.equal(
+      tet.translate(
+        "",
+        "There is %(total)s object. Remaining: %(count)s",
+        "",
+        5,
+        "",
+        { total: 5, count: 10 }
+      ),
+      "Iha 5 objetu sira. Restu mak: 10"
+    );
+  });
+});
 
-  it('should translate interpolated positional markers when present', () => {
-
-    assert.equal(tet.translate('', 'There is %(total)s object. Remaining: %(count)s', 'There are %(total)s objects. Remaining: %(count)s', 1, '', {'total': 1, 'count': 10}), 'Iha 1 objetu. Restu mak: 10');
-    assert.equal(tet.translate('', 'There is %(total)s object. Remaining: %(count)s', 'There are %(total)s objects. Remaining: %(count)s', 5, '', {'total': 5, 'count': 10}), 'Iha 5 objetu sira. Restu mak: 10');
-  })
-  it('Should not require a plural for translation', () => {
-    assert.equal(tet.translate('', 'There is %(total)s object. Remaining: %(count)s', '', 5, '', {'total': 5, 'count': 10}), 'Iha 5 objetu sira. Restu mak: 10');
-  })
-
-})
+describe("Gettext functions", function () {
+  it('gettext("Activity") => Actividade', () => {
+    assert.equal(tet.gettext("Activity"), "Actividade");
+  });
+  it('pgettext("month", "may") => Maiu', () => {
+    assert.equal(tet.pgettext("month", "may"), "maiu");
+  });
+  it("Can translate plurals (ngettext)", () => {
+    assert.equal(
+      tet.ngettext("There is one activity", "There are many activities", "1"),
+      "Iha atividade ida"
+    );
+    assert.equal(
+      tet.ngettext("There is one activity", "There are many activities", "5"),
+      "Iha atividade barak"
+    );
+  });
+  it("Can substitute positional markers: Testing interpolate functions", () => {
+    assert.equal(
+      tet.interpolate(
+        tet.ngettext(
+          "There is %(total)s object. Remaining: %(count)s",
+          "There are %(total)s objects. Remaining: %(count)s",
+          1
+        ),
+        { total: 1, count: 10 },
+        true
+      ),
+      "Iha 1 objetu. Restu mak: 10"
+    );
+    assert.equal(
+      tet.interpolate(
+        tet.ngettext(
+          "There is %s object. Remaining: %s",
+          "There are %s objects. Remaining: %s",
+          5
+        ),
+        [5, 10],
+        false
+      ),
+      "Iha 5 objetu sira. Restu mak: 10"
+    );
+  });
+});
